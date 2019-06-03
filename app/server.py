2f209f36bb51d7ebe4b5a9cdc57e7b2b2322464f
Enrico Rinaldi <erinaldi@bnl.gov>
2019-06-03 17:36:14 +0900

    updated model because it was corrupted

--- app/server.py
+++ app/server.py
@@ -9,8 +9,8 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1zyLImePeaDBRk76lHAzFu154r8dL97d6'
-export_file_name = 'resnet34_for_famous_physicists.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=10QxCdeWggEUmglr8WS1dtb1ZI7MnfAFt'
+export_file_name = 'export.pkl'
 
 classes = ['einstein', 'hawking', 'tyson']
 path = Path(__file__).parent
