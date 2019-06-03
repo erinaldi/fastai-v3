cef2d84c00fa9a10bb0d62d98a9823ed8776be20
Enrico Rinaldi <erinaldi@bnl.gov>
2019-06-03 17:20:01 +0900

    Updated GDrive link and classes.

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1zyLImePeaDBRk76lHAzFu154r8dL97d6'
+export_file_name = 'resnet34_for_famous_physicists.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['einstein', 'hawking', 'tyson']
 path = Path(__file__).parent
 
 app = Starlette()
